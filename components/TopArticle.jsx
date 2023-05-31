e3465ff97e7b049de57d6192e33abfc8a0be73b7
Dannydan <danny7898@gmail.com>
2023-05-31 16:38:18 +0200

    first commit

--- components/TopArticle.jsx
+++ components/TopArticle.jsx
@@ -1,18 +1,65 @@
-import React from 'react'
-import styles from '../styles/TopArticle.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import Image from "next/image";
+import styles from "../styles/TopArticle.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch, useSelector } from "react-redux";
+import { addBookmarks, removeBookmarks } from "@/reducers/bookmarks";
 
-export default function TopArticle() {
-    return (<>
-        <section className={styles.topContainer}>
-            <img src="./articleImg.png" alt="building" className={styles.imgTop} width={700} height={400}/>
-            <div className={styles.topText}>
-                <h2>Un Titre</h2>
-                <FontAwesomeIcon icon= {faBookmark} className={styles.bookmarkIcon}/>
-                <h4>Un auteur</h4>
-                <p>Une descritpion</p>
-            </div>
-        </section>
-    </>)
+// Code du composant TopArticle qui représente l'article principal.
+
+function TopArticle(props) {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.value);
+
+  const handleBookmarkClick = () => {
+    if (!user.token) {
+      return;
+    }
+    fetch(`http://localhost:3001/users/canBookmark/${user.token}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.result && data.canBookmark) {
+          if (props.isBookmarked) {
+            dispatch(removeBookmarks(props));
+          } else {
+            dispatch(addBookmarks(props));
+          }
+        }
+      });
+  };
+
+  let iconStyle = {};
+  if (props.isBookmarked) {
+    iconStyle = { color: "#E9BE59" };
+  } else {
+    iconStyle = { color: "#000000" };
+  }
+
+  return (
+    <>
+      <div className={styles.topContainer}>
+        <Image
+          src={props.urlToImage}
+          alt={props.title}
+          width={700}
+          height={400}
+        />
+        <div className={styles.topText}>
+          <h2>{props.title}</h2>
+          <FontAwesomeIcon
+            style={iconStyle}
+            icon={faBookmark}
+            className={styles.bookmarkIcon}
+            onClick={() => handleBookmarkClick()}
+          />
+
+          <h4>{props.author}</h4>
+          <p>{props.description}</p>
+        </div>
+      </div>
+    </>
+  );
 }
+
+export default TopArticle;
